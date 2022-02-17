178d71745bd14263efd7bb3b88d41052403c8849
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-17 21:21:48 +0700

    import { useState } from 'react';

--- router-tutorial/src/components/quotes/NoQuotesFound.js
+++ router-tutorial/src/components/quotes/NoQuotesFound.js
@@ -1,10 +1,14 @@
-import classes from "./NoQuotesFound.module.css";
+import classes from './NoQuotesFound.module.css';
 
 const NoQuotesFound = () => {
   return (
     <div className={classes.noquotes}>
-      <p>No quotes founds</p>
-      <a className={classes.btn}>Add a Quote</a>
+      <p>No quotes found!</p>
+      <a className='btn'>
+        Add a Quote
+      </a>
     </div>
   );
 };
+
+export default NoQuotesFound;
