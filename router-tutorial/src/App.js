d685a8fac54a7116f0ee3f5308dc31d893dcbbab
Le Hong vo <volhfx07132@funix.edu.vn>
2022-02-18 21:07:31 +0700

    Set account

--- router-tutorial/src/App.js
+++ router-tutorial/src/App.js
@@ -1,8 +1,28 @@
+import { Router, Switch, Redirect } from "react-router-dom";
+import AllQuotes from "./pages/AllQuotes";
+import NewQuote from "./pages/NewQuote";
+import QuoteDetail from "./pages/QuoteDetail";
+//BAP acccount: volh@bap.jp
+//BAP password: BAP@2020!pXMW6V22
 function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
-    </div>
+    <Switch>
+      <Router path="/" exact>
+        <Redirect path="/quotes" />
+      </Router>
+
+      <Router path="/quotes" exact>
+        <AllQuotes />
+      </Router>
+
+      <Router path="/quotes/:quoteId">
+        <QuoteDetail />
+      </Router>
+
+      <Router path="/new-quote">
+        <NewQuote />
+      </Router>
+    </Switch>
   );
 }
 
